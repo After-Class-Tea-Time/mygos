2fe1ae91f51379106ce73f90c5e64576768989c7
Kuuhaku_kazari <SP_YYY@outlook.com>
2025-06-06 20:06:22 +0800

    修改

--- include/typedefs.h
+++ include/typedefs.h
@@ -4,14 +4,14 @@
 
 #ifndef TYPEDEFS_H
 #define TYPEDEFS_H
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
+typedef unsigned char      uint8_t;
+typedef unsigned short     uint16_t;
+typedef unsigned int       uint32_t;
 typedef unsigned long long uint64_t;
-typedef signed char int8_t;
-typedef signed short int16_t;
-typedef signed int int32_t;
-typedef signed long long int64_t;
+typedef signed char        int8_t;
+typedef signed short       int16_t;
+typedef signed int         int32_t;
+typedef signed long long   int64_t;
 typedef unsigned long long uintptr_t;
 /*/
  * Register Width
@@ -20,11 +20,8 @@ typedef uint32_t reg_t;
 typedef uint32_t ptr_t;
 
 
-#define NULL ((void*)0)
-
-
 typedef struct timer* timer_ptr; // 计时器指针类型
 #define TRUE 1
 #define FALSE 0
-#define NULL ((void *)0)
-#endif //TYPEDEFS_H
+#define NULL ((void*)0)
+#endif // TYPEDEFS_H
